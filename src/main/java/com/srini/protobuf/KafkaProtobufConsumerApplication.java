c5012f4b8f3bac9e7a680ef60b5c194decef7425
Srini <srinivasboini7@gmail.com>
2023-05-13 17:10:01 +0800

    Initial commit

---
+++ src/main/java/com/srini/protobuf/KafkaProtobufConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.srini.protobuf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProtobufConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaProtobufConsumerApplication.class, args);
+	}
+
+}
