2cfa524d05972d0a401e987da72f5c566beb0b89
Srini <srinivasboini7@gmail.com>
2023-05-13 17:17:37 +0800

    consumer configs added

--- src/main/java/com/srini/protobuf/KafkaProtobufConsumerApplication.java
+++ src/main/java/com/srini/protobuf/KafkaProtobufConsumerApplication.java
@@ -2,8 +2,10 @@ package com.srini.protobuf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableKafka
 public class KafkaProtobufConsumerApplication {
 
 	public static void main(String[] args) {
